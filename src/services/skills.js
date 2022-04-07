014b1c439035add992ba6ed4be749c518975bead
Lucas Pontes Arruda <paluke94@gmail.com>
2022-04-07 18:07:56 -0300

    [feat] add skills service

---
+++ src/services/skills.js
@@ -0,0 +1,64 @@
+const SKILLS = [
+  {
+    id: 1,
+    name: 'HTML',
+    icon: 'devicon-html5-plain',
+  },
+  {
+    id: 2,
+    name: 'CSS',
+    icon: 'devicon-css3-plain',
+  },
+  {
+    id: 3,
+    name: 'JavaScript',
+    icon: 'devicon-javascript-plain',
+  },
+  {
+    id: 4,
+    name: 'React',
+    icon: 'devicon-react-original',
+  },
+  {
+    id: 5,
+    name: 'Redux',
+    icon: 'devicon-redux-original',
+  },
+  {
+    id: 6,
+    name: 'Jest',
+    icon: 'devicon-jest-plain',
+  },
+  {
+    id: 7,
+    name: 'TypeScript',
+    icon: 'devicon-typescript-plain',
+  },
+  {
+    id: 8,
+    name: 'MySQL',
+    icon: 'devicon-mysql-plain',
+  },
+  {
+    id: 9,
+    name: 'Express',
+    icon: 'devicon-express-original',
+  },
+  {
+    id: 10,
+    name: 'Git',
+    icon: 'devicon-git-plain',
+  },
+  {
+    id: 11,
+    name: 'GitHub',
+    icon: 'devicon-github-original',
+  },
+  {
+    id: 12,
+    name: 'Trello',
+    icon: 'devicon-trello-plain',
+  },
+];
+
+export default SKILLS;
