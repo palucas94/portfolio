779a6c05cef0b70a4cd8ebccd7e6f30580c03022
Lucas Pontes Arruda <paluke94@gmail.com>
2022-04-08 16:09:48 -0300

    [feat] change NavBar navigation to react-scroll

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <a href="#about" className="nav-link" data-aos="fade-right">ABOUT</a>
-      <a href="#skills" className="nav-link" data-aos="fade-left">SKILLS</a>
-      <a href="#projects" className="nav-link" data-aos="fade-right">PROJECTS</a>
-      <a href="#contact" className="nav-link" data-aos="fade-left">CONTACT</a>
+      <Link smooth to="about" className="nav-link" data-aos="fade-right">ABOUT</Link>
+      <Link smooth to="skills" className="nav-link" data-aos="fade-left">SKILLS</Link>
+      <Link smooth to="projects" className="nav-link" data-aos="fade-right">PROJECTS</Link>
+      <Link smooth to="contact" className="nav-link" data-aos="fade-left">CONTACT</Link>
     </nav>
   );
 }
