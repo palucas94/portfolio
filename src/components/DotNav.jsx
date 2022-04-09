cac790e9d68c2500bfa698bb6632b87763bbef31
Lucas Pontes Arruda <paluke94@gmail.com>
2022-04-09 09:55:10 -0300

    [styles] add DotNav animation

--- src/components/DotNav.jsx
+++ src/components/DotNav.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 function DotNav({ page }) {
   return (
-    <div className="dotnav">
+    <div className="dotnav" data-aos="flip-up">
       <Link smooth to="home" className={`${page === 'home' ? 'dotnav-selected' : 'dotnav-link'}`} />
       <Link smooth to="about" className={`${page === 'about' ? 'dotnav-selected' : 'dotnav-link'}`} />
       <Link smooth to="skills" className={`${page === 'skills' ? 'dotnav-selected' : 'dotnav-link'}`} />
